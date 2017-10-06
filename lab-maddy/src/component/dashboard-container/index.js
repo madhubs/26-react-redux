06c29c2d7d9b367fd405f9077ca31c06f1f6c9a8
Madeline Stevens <stevensm1987@gmail.com>
2017-10-05 22:26:25 -0700

    woohoo delete and update category name both work add card to category does not work yet

--- lab-maddy/src/component/dashboard-container/index.js
+++ lab-maddy/src/component/dashboard-container/index.js
@@ -1,15 +1,10 @@
-// ###### Dashboard Component
-// * should be displayed on the `/` route
-// * should use react-redux's `connect` to map state and dispatch methods to props
-// * should display a `CategoryForm` for adding categories to the application state
-// * should display a `CategoryItem` for each category in the application state
-
 import React from 'react';
 import './dashboard-container.scss';
 import {connect} from 'react-redux'; //importing a binding ({connect}) of react-redux called react-redux. The react bindings. Grabbing state and dispatching.
 import {categoryCreate} from '../../action/category-actions';
 import CategoryForm from '../category-form';
 import CategoryItem from '../category-item';
+
 
 class DashboardContainer extends React.Component {
   componentDidMount() {
@@ -22,23 +17,26 @@ class DashboardContainer extends React.Component {
   render() {
     return (
       <main className="main-content">
-        <h2>Expense Tracker</h2>
+        <h2>Dashboard</h2>
 
         <CategoryForm
-           buttonText='create expense'
-           onComplete={this.props.categoryCreate}/>
+          buttonText="create"
+          toggle={() => {}}
+          onComplete={this.props.categoryCreate}/>
 
-         {this.props.categories.length ?
-           <div>
-            {this.props.categories.map(item => {
-              return <CategoryItem
-                       key={item.id}
-                       category={item}/>;
-            })}
-         </div> :
-         <h2>Add some categories</h2>
-       }
-       </main>
+        <div className="category-container">
+          {this.props.categories.length ?
+            <div>
+              {this.props.categories.map(item => {
+                return <CategoryItem
+                          key={item.id}
+                          category={item}/>;
+              })}
+            </div> :
+            <h2>Add some categories</h2>
+          }
+        </div>
+      </main>
     );
   }
 }
@@ -47,7 +45,6 @@ class DashboardContainer extends React.Component {
 const mapStateToProps = state => {
   return {
     categories: state.categories,
-    cards: state.cards,
   };
 };
 
